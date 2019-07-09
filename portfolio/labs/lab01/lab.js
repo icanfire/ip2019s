934a740c9e7aef9c3a6c16da7f4a1c6500212054
icanfire <52520988+icanfire@users.noreply.github.com>
2019-07-09 08:59:30 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,40 +4,7 @@
 
 
 function start(e) {
- var name =[
-            {
-                char:'郭',
-                big5:'B3A2',
-                cns:'1-5D66',
-                unicode:'90ED'
-            },
-            {
-                char:'秉',
-                big5:'AAC3',
-                cns:'1-4F26',
-                unicode:'79C9'
-            },
-            {
-                char:'鈞',
-                big5:'B676',
-                cns:'1-625D',
-                unicode:'921E'
-            }
-            ];
 
-
- var ele =document.getElementById("div1");
-
-  for (var i= 0;i<3;i++) {
-   ele.innerHTML += name[i].char;
-    ele.innerHTML += 'big5=';
-    ele.innerHTML += name[i].big5;
-    ele.innerHTML +='cns=';
-    ele.innerHTML +=name[i].cns
-    ele.innerHTML +='unicode=';
-    ele.innerHTML +=name[i].unicode;
-    ele.innerHTML +='<br>';
-  }
 }
 
 /*
